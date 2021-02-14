1ab0bec17c624338face36fc2a0aba830f0dd427
yutingh <yutingh@cw.com.tw>
2021-02-14 23:47:18 +0800

    手機版限定

--- artistry/assets/js/main.js
+++ artistry/assets/js/main.js
@@ -2,14 +2,16 @@ $(function () {
 	var width = $(window).width(),
 		height = $(window).height();
 	new WOW().init();
-	$(".hamburger").on("click", function () {
-		$(this).toggleClass("active");
-		$("nav").animate({ width: "toggle" }, 500);
-	});
-	$("nav ul li a").on("click", function () {
-		$('.hamburger').toggleClass("active");
-		$("nav").animate({ width: "toggle" }, 500);
-	});
+	if (width < 1024) {
+		$(".hamburger").on("click", function () {
+			$(this).toggleClass("active");
+			$("nav").animate({ width: "toggle" }, 500);
+		});
+		$("nav ul li a").on("click", function () {
+			$('.hamburger').toggleClass("active");
+			$("nav").animate({ width: "toggle" }, 500);
+		});
+	}
 	$('a.smooth-scroll[href*="#"]:not([href="#"])').on("click", function() {
 		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 			var target = $(this.hash);
