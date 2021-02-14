35ec3c82aec89e03c63a9080217e8dd902c760a9
yutingh <yutingh@cw.com.tw>
2021-02-14 23:44:21 +0800

    artistry new

---
+++ artistry/assets/js/main.js
@@ -0,0 +1,35 @@
+$(function () {
+	var width = $(window).width(),
+		height = $(window).height();
+	new WOW().init();
+	$(".hamburger").on("click", function () {
+		$(this).toggleClass("active");
+		$("nav").animate({ width: "toggle" }, 500);
+	});
+	$("nav ul li a").on("click", function () {
+		$('.hamburger').toggleClass("active");
+		$("nav").animate({ width: "toggle" }, 500);
+	});
+	$('a.smooth-scroll[href*="#"]:not([href="#"])').on("click", function() {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: (target.offset().top - (height / 10))
+				}, 1000);
+				return false;
+			}
+		}
+	});
+	$(window).resize(function () {});
+	$(window).scroll(function () {
+		var scroll = $(this).scrollTop(),
+			openingHeight = $(".opening").outerHeight();
+		if (scroll >= openingHeight / 2) {
+			$("a.gotop").fadeIn();
+		} else {
+			$("a.gotop").fadeOut(100);
+		}
+	});
+});
