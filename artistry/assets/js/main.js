7415e94dbc75550a8e141017750a582d6db524bd
yutingh <yutingh@cw.com.tw>
2021-02-22 00:44:35 +0800

    手機版+出現互動

--- artistry/assets/js/main.js
+++ artistry/assets/js/main.js
@@ -24,6 +24,20 @@ $(function () {
 			}
 		}
 	});
+	$('.force-more').click(function(){
+		$(this).toggleClass('opened')
+		$(this).parent().siblings('p').slideToggle(300);
+		$(this).parent().parent('.col-md-4').siblings('.col-md-4').children('.col-title').children('.force-more').removeClass('opened');
+		$(this).parent().parent('.col-md-4').siblings('.col-md-4').children('p').slideUp();
+	})
+	$('.product-more').click(function(){
+		$(this).toggleClass('opened')
+		$(this).parent().siblings('p').slideToggle(300);
+		$(this).parent().parent('.product-center').siblings('.product-bottom').slideToggle(300);
+		$(this).parent().parent().parent().parent().siblings().children('.product-body').children('.product-center').children().children('.product-more').removeClass('opened')
+		$(this).parent().parent().parent().parent().siblings().children('.product-body').children('.product-center').children('p').slideUp();
+		$(this).parent().parent().parent().parent().siblings().children('.product-body').children('.product-bottom').slideUp();
+	})
 	$(window).resize(function () {});
 	$(window).scroll(function () {
 		var scroll = $(this).scrollTop(),
